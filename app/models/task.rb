41dabe934b621397d0e7031a77ca09979748e026
she <fangfangshe09@gmail.com>
2023-05-04 23:08:47 +0900

    system specのテスト

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,7 @@
 class Task < ApplicationRecord
   validates :title, length: { maximum: 50 }, presence: true
   validates :content, length: { maximum: 1000 }, presence: true
-  validates :deadline, :priority, :status, presence: true
+  # validates :deadline, :priority, :status, presence: true
 
   enum status: { 未着手: 0, 着手中: 1, 完了: 2 }
   enum priority: { 高: 0, 中: 1, 低: 2 }
