7cbe62add46f481bcb43c428865d78b2ea89f10a
she <fangfangshe09@gmail.com>
2023-05-05 17:13:34 +0900

    タスク一覧を作成日時の降順

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,8 @@ class TasksController < ApplicationController
 
   def index
     @tasks = Task.all  
+    @tasks = Task.order(created_at: :desc)
+
   end
 
   def new
--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,8 @@ require "rails/all"
 Bundler.require(*Rails.groups)
 module IntotaskApp
   class Application < Rails::Application
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.load_defaults 6.1
     config.generators do |g|
       g.test_framework :rspec,
