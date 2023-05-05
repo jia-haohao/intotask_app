f1d231ef82034cb76203f124694f397df33a7cf7
she <fangfangshe09@gmail.com>
2023-05-05 19:37:50 +0900

    herokuへデプロイの手順

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,4 +1,7 @@
+# 「FactoryBotを使用します」という記述
 FactoryBot.define do
+  # 作成するテストデータの名前を「task」とします
+  # （実際に存在するクラス名と一致するテストデータの名前をつければ、そのクラスのテストデータを自動で作成します）
   factory :task do
     title { 'task_title' }
     content { 'task_content' }
@@ -8,11 +11,11 @@ FactoryBot.define do
   end
   # 作成するテストデータの名前を「second_task」とします
   # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
-  # factory :second_task, class: Task do
-  #   title { 'Factoryで作ったデフォルトのタイトル２' }
-  #   content { 'Factoryで作ったデフォルトのコンテント２' }
-  #   priority { 2 }
+  factory :second_task, class: Task do
+    title { 'task_title1' }
+    content { 'task_content1' }
+    # priority { 2 }
   #   status { '着手' }
   #   deadline { '2013-5-30' }
-  # end
+  end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -6,11 +6,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in "task_title", with: 'task'
         fill_in "task_content", with: '卒業発表会'
-        click_on 'Create Task'
+        click_on '登録する'
         expect(page).to have_content 'task'
         expect(page).to have_content '卒業発表会'
       end
     end
+  end
+  before do
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
   end
   describe '一覧表示機能' do
     context '一覧画面に遷移した場合' do
@@ -18,6 +22,15 @@ RSpec.describe 'タスク管理機能', type: :system do
         FactoryBot.create(:task, title: 'タイトル',  content: '書類作成')
         visit tasks_path
         expect(page).to have_content '書類作成'
+      end
+    end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        visit tasks_path
+        task_list = all('.task_list') 
+        expect(task_list[0]).to have_content 'task2'
+        expect(task_list[1]).to have_content 'task1'
       end
     end
   end
